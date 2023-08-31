7edafe10fffaf9bce16cfb1197031c07293412de
adoumasseo <adoumasseo@gmail.com>
2023-08-31 11:25:23 +0100

    Test arrays and pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in rervese order
+ * @s: the string tha gonna be print
+ *
+ * Description: Nothing special same as title description
+ * Return: nothing it's void type function
+ */
+
+void print_rev(char *s)
+{
+	int len, i;
+
+
+}
--- 0x05-pointers_arrays_strings/test.c
+++ 0x05-pointers_arrays_strings/test.c
@@ -3,6 +3,9 @@
 void main(void)
 {
 	char *a;
+	char *w = "withney est très belle nous aurons de beaux gosses";
+	printf("%ls\n", i);
 	a = "je suis donc trop beau";
 	printf("%s", a);
+	printf("%s", w);
 }
