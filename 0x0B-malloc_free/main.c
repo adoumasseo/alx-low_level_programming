cc28de9b57af4f5ddd85fbabdee632430a16d463
adoumasseo <adoumasseo@gmail.com>
2023-10-09 10:01:29 +0100

    alx tasks

--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -1,55 +1,23 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-int **alloc_grid(int, int);
-void free_grid(int **grid, int height);
 /**
- * print_grid - prints a grid of integers
- * @grid: the address of the two dimensional grid
- * @width: width of the grid
- * @height: height of the grid
- *
- * Return: Nothing.
- */
-void print_grid(int **grid, int width, int height)
-{
-	int w;
-	int h;
-
-	h = 0;
-	while (h < height)
-	{
-		w = 0;
-		while (w < width)
-		{
-			printf("%d ", grid[h][w]);
-			w++;
-		}
-		printf("\n");
-		h++;
-	}
-}
-
-/**
- * main - check the code .
+ * main - check the code for ALX School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int **grid;
-	int h;
-	int w;
+    char *s;
 
-	h = 20;
-	w = 20;
-	grid = alloc_grid(h, w);
-	if (grid == NULL)
-	{
-		return (1);
-	}
-	print_grid(grid, h, w);
-	printf("\n");
-	free(grid);
-	return (0);
+    s = str_concat("Betty ", "Holberton");
+    if (s == NULL)
+    {
+        printf("failed\n");
+        return (1);
+    }
+    printf("%s\n", s);
+    free(s);
+    return (0);
 }
