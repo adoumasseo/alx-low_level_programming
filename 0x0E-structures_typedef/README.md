67322ef29f6addff3e2d834f96826c6558bfc80c
adoumasseo <adoumasseo@gmail.com>
2023-11-06 17:53:31 +0100

    struct ALX tasks

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+it's about struct project in ALX SE programs
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - definition of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of that dog
+ *
+ * Description: description of any dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
